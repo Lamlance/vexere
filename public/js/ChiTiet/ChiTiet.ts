5988ada1ca0c5d7b58046ad6999b94e89e419454
lamlance <lamhoangdien113@gmail.com>
2022-11-20 23:58:59 +0700

    Change css class name to English

--- public/js/ChiTiet/ChiTiet.ts
+++ public/js/ChiTiet/ChiTiet.ts
@@ -1,12 +1,12 @@
 //Chi tiet template maker
-interface imgAtribute{
-  src:string,
-  alt:string
+interface imgAtribute {
+  src: string,
+  alt: string
 }
-const makeImgSrcArray = ()=>{
+const makeImgSrcArray = () => {
   const arr = Array<imgAtribute>(5);
   for (let index = 0; index < arr.length; index++) {
-    const data:imgAtribute = {
+    const data: imgAtribute = {
       src: `image/image${index}.png`,
       alt: `car img no image${index}`
     }
@@ -17,15 +17,15 @@ const makeImgSrcArray = ()=>{
 
 class ChiTietElement extends HTMLElement {
 
-  displayChiTiet:boolean;
+  displayChiTiet: boolean;
   detailTabs: NodeListOf<HTMLElement>;
-  currentDisplayedDetailTab:number;
+  currentDisplayedDetailTab: number;
 
   constructor() {
     super();
     this.displayChiTiet = false;
     this.attachShadow({ mode: "open" });
-    
+
     let template = <HTMLTemplateElement>document.getElementById("chi-tiet-template");
 
     const styleLinkElem = document.createElement("link");
@@ -36,27 +36,27 @@ class ChiTietElement extends HTMLElement {
     let templateContent = template.content;
     this.shadowRoot?.appendChild(styleLinkElem);
     this.shadowRoot?.appendChild(templateContent.cloneNode(true));
-    
-    this.detailTabs = <NodeListOf<HTMLElement>> this.shadowRoot?.querySelectorAll(".vexere_chitiet_tab_item");
+
+    this.detailTabs = <NodeListOf<HTMLElement>>this.shadowRoot?.querySelectorAll(".js_vexere_detail_tab_item");
     this.currentDisplayedDetailTab = 0;
-    
+
 
     this.addDetailImage();
     this.addOpenDatailClickHandle();
     this.addDetailTabClickHandle();
   }
 
-  addDetailImage(){
-    const imgDisplayWrapper = this.shadowRoot?.querySelector(".vexere_hinhxe_slider_display");
+  addDetailImage() {
+    const imgDisplayWrapper = this.shadowRoot?.querySelector(".vexere_bus_image_slider_display");
     const imgDatas = makeImgSrcArray();
 
-    imgDatas.forEach((item)=>{
+    imgDatas.forEach((item) => {
       const imgDivWrap = document.createElement("div");
       const image = document.createElement("img");
       image.src = item.src;
       image.alt = item.alt;
 
-      imgDivWrap.className = "vexere_hinhxe_slider_display_img";
+      imgDivWrap.className = "vexere_bus_image_slider_display_img";
       imgDivWrap.appendChild(image);
 
       imgDisplayWrapper?.appendChild(imgDivWrap);
@@ -64,32 +64,30 @@ class ChiTietElement extends HTMLElement {
 
   }
 
-  addOpenDatailClickHandle(){
-    const chiTietAction =  this.shadowRoot?.querySelector(".vexere_thongtin_action > h4");
-    const chiTiet = <HTMLElement> this.shadowRoot?.querySelector(".vexere_bang_chi_tiet") ;
+  addOpenDatailClickHandle() {
+    const chiTietAction = this.shadowRoot?.querySelector(".vexere_info_action > h4");
+    const chiTiet = <HTMLElement>this.shadowRoot?.querySelector(".vexere_detail_info_container");
     chiTiet.style.display = "none";
 
-    chiTietAction?.addEventListener('click',()=>{
+    chiTietAction?.addEventListener('click', () => {
       this.displayChiTiet = !this.displayChiTiet;
-      const chiTietElement = <HTMLElement> this.shadowRoot?.querySelector(".vexere_bang_chi_tiet") ;
+      const chiTietElement = <HTMLElement>this.shadowRoot?.querySelector(".vexere_detail_info_container");
       chiTietElement.style.display = (this.displayChiTiet) ? "" : "none";
     });
   }
 
-  addDetailTabClickHandle(){
-    this.detailTabs.forEach((ele,index)=>{
-      if(index != 0){
-        ele.style.display = "none";
-      }
+  addDetailTabClickHandle() {
+    this.detailTabs.forEach((ele, index) => {
+      ele.style.display = (index != 0) ? "none" : "";
     });
 
-    const buttons = this.shadowRoot?.querySelectorAll(".vexere_chitiet_tab > button") ;
-    if(buttons){
-      buttons.forEach((btn)=>{
-        btn.addEventListener("click",(event)=>{
+    const buttons = this.shadowRoot?.querySelectorAll(".vexere_detail_info_select_tab > button");
+    if (buttons) {
+      buttons.forEach((btn) => {
+        btn.addEventListener("click", (event) => {
           const btnClickValue = Number.parseInt((<HTMLButtonElement>event.target).value);
-          if(!isNaN(btnClickValue) && btnClickValue < this.detailTabs.length){
-            this.detailTabs.forEach((ele,index)=>{
+          if (!isNaN(btnClickValue) && btnClickValue < this.detailTabs.length) {
+            this.detailTabs.forEach((ele, index) => {
               ele.style.display = "none";
             });
             this.detailTabs[btnClickValue].style.display = "";
@@ -101,10 +99,10 @@ class ChiTietElement extends HTMLElement {
 
 }
 
-customElements.define("chi-tiet-element",ChiTietElement);
+customElements.define("chi-tiet-element", ChiTietElement);
 
 const body = document.body;
-Array<number>(5).fill(0).forEach(()=>{
+Array<number>(5).fill(0).forEach(() => {
   const chiTietElement = document.createElement("chi-tiet-element");
   body.appendChild(chiTietElement);
 });
