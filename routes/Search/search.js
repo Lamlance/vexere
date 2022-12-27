54219866fad34118533afb1bf54d30b4c44e5e65
Lamlance <lamhoangdien113@gmail.com>
2022-12-27 15:31:25 +0700

    handle Search with word

--- routes/Search/search.js
+++ routes/Search/search.js
@@ -1,5 +1,6 @@
-import { singleIntQueryHandler } from "../db/queryHandler";
+import { singleIntQueryHandler, singleQueryHandler } from "../db/queryHandler";
 import { searchRouteFromDB } from "../db/searchRoute";
+import { prisma } from "../../server";
 const searchRouteHandler = async (req, res) => {
     console.log(req.query.busHouse);
     if (!(req.query && req.query.fromId && req.query.toId)) {
@@ -7,8 +8,26 @@ const searchRouteHandler = async (req, res) => {
         return;
     }
     const page = singleIntQueryHandler(req.query.page, 0);
-    const fromId = singleIntQueryHandler(req.query.fromId);
-    const toId = singleIntQueryHandler(req.query.toId);
+    let fromId = singleIntQueryHandler(req.query.fromId, NaN);
+    let toId = singleIntQueryHandler(req.query.toId, NaN);
+    if (isNaN(fromId) && isNaN(toId)) {
+        await prisma.$connect();
+        const fromLoc = await prisma.location.findMany({
+            take: 2,
+            where: {
+                OR: [
+                    { name: { contains: singleQueryHandler(req.query.fromId) } },
+                    { name: { contains: singleQueryHandler(req.query.toId) } }
+                ],
+            }
+        });
+        if (fromLoc.length !== 2) {
+            res.render("search", {});
+            return;
+        }
+        fromId = fromLoc[0].id;
+        toId = fromLoc[1].id;
+    }
     const ans = await searchRouteFromDB(fromId, toId, page);
     ans.routeDetail.forEach((detail) => {
         detail.startTime = detail.startTime.toLocaleString();
