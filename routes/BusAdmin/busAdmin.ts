82cc7b7c4ca85c822e23f4b22c347d4bf19bc276
Phu Han <phuhanld@gmail.com>
2022-12-30 12:36:53 +0700

    add initial prototype for CRUD

--- routes/BusAdmin/busAdmin.ts
+++ routes/BusAdmin/busAdmin.ts
@@ -7,4 +7,38 @@ const busAdminHandler = async (req: Request, res: Response) => {
     res.redirect("/login");
     return;
   }
+
+  // const data = await prisma.bus.findMany
+
+  // res.locals.title = "Thông tin xe";
+  // let busList: Array<Object> = [];
 };
+
+export const addBusAdminHandler = (req: Request, res: Response) => {
+  if (!req.oidc.isAuthenticated() || !req.oidc.user || !req.oidc.user.sub) {
+    res.redirect("/login");
+    return;
+  }
+};
+
+export const editBusAdminHanlder = async (req: Request, res: Response) => {
+  if (!req.oidc.isAuthenticated() || !req.oidc.user || !req.oidc.user.sub) {
+    res.redirect("/login");
+    return;
+  }
+};
+
+export const deleteBusAdminHanlder = async (req: Request, res: Response) => {
+  if (!req.oidc.isAuthenticated() || !req.oidc.user || !req.oidc.user.sub) {
+    res.redirect("/login");
+    return;
+  }
+};
+
+export const addBus = async (req: Request, res: Response) => {};
+
+export const editBus = async (req: Request, res: Response) => {};
+
+export const deleteBus = async (req: Request, res: Response) => {};
+
+export default busAdminHandler;
